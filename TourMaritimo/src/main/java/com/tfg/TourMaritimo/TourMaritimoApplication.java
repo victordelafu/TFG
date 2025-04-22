de8517a5e862867fee5276c35798f9db839be264
Victor de la Fuente <160121458+victordelafu@users.noreply.github.com>
2025-04-22 09:01:04 +0200

    Proyecto estructurado

---
+++ TourMaritimo/src/main/java/com/tfg/TourMaritimo/TourMaritimoApplication.java
@@ -0,0 +1,13 @@
+package com.tfg.TourMaritimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourMaritimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourMaritimoApplication.class, args);
+	}
+
+}
